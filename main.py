bc2b2b8eaa4bee1db43812b7cec7b5445dd3c62e
xiaoluo999 <gaoliang1054@163.com>
2018-07-03 22:02:26 +0800

    version1.0

---
+++ main.py
@@ -0,0 +1,100 @@
+#Author:xiao luo
+import cv2
+import tensorflow as tf
+import numpy as np
+from tensorflow.examples.tutorials.mnist import input_data
+#定义一个生成器，
+def generator(z, batch_size, z_dim):
+    g_w1 = tf.get_variable('g_w1', [z_dim, 3136], dtype=tf.float32, initializer=tf.truncated_normal_initializer(stddev=0.02))
+    g_b1 = tf.get_variable('g_b1', [3136], initializer=tf.truncated_normal_initializer(stddev=0.02))
+    g1 = tf.matmul(z, g_w1) + g_b1
+    g1 = tf.reshape(g1, [-1, 56, 56, 1])
+    g1 = tf.contrib.layers.batch_norm(g1, epsilon=1e-5, scope='bn1')
+    g1 = tf.nn.relu(g1)
+
+    # Generate 50 features
+    g_w2 = tf.get_variable('g_w2', [3, 3, 1, z_dim/2], dtype=tf.float32, initializer=tf.truncated_normal_initializer(stddev=0.02))
+    g_b2 = tf.get_variable('g_b2', [z_dim/2], initializer=tf.truncated_normal_initializer(stddev=0.02))
+    g2 = tf.nn.conv2d(g1, g_w2, strides=[1, 2, 2, 1], padding='SAME')
+    g2 = g2 + g_b2
+    g2 = tf.contrib.layers.batch_norm(g2, epsilon=1e-5, scope='bn2')
+    g2 = tf.nn.relu(g2)
+    g2 = tf.image.resize_images(g2, [56, 56])#图像归一化56*56
+
+    # Generate 25 features
+    g_w3 = tf.get_variable('g_w3', [3, 3, z_dim/2, z_dim/4], dtype=tf.float32, initializer=tf.truncated_normal_initializer(stddev=0.02))
+    g_b3 = tf.get_variable('g_b3', [z_dim/4], initializer=tf.truncated_normal_initializer(stddev=0.02))
+    g3 = tf.nn.conv2d(g2, g_w3, strides=[1, 2, 2, 1], padding='SAME')
+    g3 = g3 + g_b3
+    g3 = tf.contrib.layers.batch_norm(g3, epsilon=1e-5, scope='bn3')
+    g3 = tf.nn.relu(g3)
+    g3 = tf.image.resize_images(g3, [56, 56])
+
+    # Final convolution with one output channel
+    g_w4 = tf.get_variable('g_w4', [1, 1, z_dim/4, 1], dtype=tf.float32, initializer=tf.truncated_normal_initializer(stddev=0.02))
+    g_b4 = tf.get_variable('g_b4', [1], initializer=tf.truncated_normal_initializer(stddev=0.02))
+    g4 = tf.nn.conv2d(g3, g_w4, strides=[1, 2, 2, 1], padding='SAME')
+    g4 = g4 + g_b4
+    g4 = tf.sigmoid(g4)
+
+    # Dimensions of g4: batch_size x 28 x 28 x 1
+    return g4
+#定义判别器，输出一个概率
+def discriminator(images, reuse=False):
+    if (reuse):
+        tf.get_variable_scope().reuse_variables()
+
+    # First convolutional and pool layers
+    # This finds 32 different 5 x 5 pixel features
+    d_w1 = tf.get_variable('d_w1', [5, 5, 1, 32], initializer=tf.truncated_normal_initializer(stddev=0.02))
+    d_b1 = tf.get_variable('d_b1', [32], initializer=tf.constant_initializer(0))
+    d1 = tf.nn.conv2d(input=images, filter=d_w1, strides=[1, 1, 1, 1], padding='SAME')
+    d1 = d1 + d_b1
+    d1 = tf.nn.relu(d1)
+    d1 = tf.nn.avg_pool(d1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+    # Second convolutional and pool layers
+    # This finds 64 different 5 x 5 pixel features
+    d_w2 = tf.get_variable('d_w2', [5, 5, 32, 64], initializer=tf.truncated_normal_initializer(stddev=0.02))
+    d_b2 = tf.get_variable('d_b2', [64], initializer=tf.constant_initializer(0))
+    d2 = tf.nn.conv2d(input=d1, filter=d_w2, strides=[1, 1, 1, 1], padding='SAME')
+    d2 = d2 + d_b2
+    d2 = tf.nn.relu(d2)
+    d2 = tf.nn.avg_pool(d2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+    # First fully connected layer
+    d_w3 = tf.get_variable('d_w3', [7 * 7 * 64, 1024], initializer=tf.truncated_normal_initializer(stddev=0.02))
+    d_b3 = tf.get_variable('d_b3', [1024], initializer=tf.constant_initializer(0))
+    d3 = tf.reshape(d2, [-1, 7 * 7 * 64])
+    d3 = tf.matmul(d3, d_w3)
+    d3 = d3 + d_b3
+    d3 = tf.nn.relu(d3)
+
+    # Second fully connected layer
+    d_w4 = tf.get_variable('d_w4', [1024, 1], initializer=tf.truncated_normal_initializer(stddev=0.02))
+    d_b4 = tf.get_variable('d_b4', [1], initializer=tf.constant_initializer(0))
+    d4 = tf.matmul(d3, d_w4) + d_b4
+
+    # d4 contains unscaled values
+    return d4
+if __name__ == "__main__":
+    mnist = input_data.read_data_sets("I:\mnist")
+    image = mnist.train.next_batch(1)[0]
+    image = tf.reshape(image,(28,28))
+    len=100#数据长度
+    origin_data= tf.random_normal(shape=(1,len))
+    output = generator(origin_data,1,len)
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        gennerate_image = sess.run(output)
+        gennerate_image = np.resize(gennerate_image,(28,28))
+        cv2.imshow("gennerate",gennerate_image)
+        cv2.imwrite("gennerate.bmp",gennerate_image)
+        cv2.waitKey(0)
+    #discriminator(image,False)
+#显示图像
+# with tf.Session() as sess:
+#     temp = sess.run(image)
+#     cv2.imshow("image",temp)
+
+print("tensorflow")
